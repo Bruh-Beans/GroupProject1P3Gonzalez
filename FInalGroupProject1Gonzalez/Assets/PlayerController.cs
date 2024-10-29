7ec6e566a8d6802bc768bddaeb1d91a29778e520
Bruh-Beans <145381152+Bruh-Beans@users.noreply.github.com>
2024-10-28 22:56:48 -0700

    Merged movement scripts into 1
    Also added special jump feature where when you hold down you jump heigher but when you do 2 quick presses you skip in a way, like a bunny hop.

--- FInalGroupProject1Gonzalez/Assets/PlayerController.cs
+++ FInalGroupProject1Gonzalez/Assets/PlayerController.cs
@@ -3,33 +3,53 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class Moving : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     private float horizontal;
     private float speed = 8f;
-    private float jumpingPower = 16f;
     private bool isFacingRight = true;
+    private bool isDead = false;
+    private int jumpCount = 0;  // Track number of jumps
+
     public TextMeshProUGUI gameOverText;
+    public float upForce = 200f; // Adjustable in-game
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        if (!isDead) // Only allow actions if player is not dead
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-        }
+            horizontal = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-        }
+            // Check if grounded to reset jump count
+            if (IsGrounded())
+            {
+                jumpCount = 0; // Reset jumps when player touches the ground
+            }
 
-        Flip();
+            // Double jump when pressing Space, only if jumpCount is less than 2
+            if (Input.GetKeyDown(KeyCode.Space) && jumpCount < 2)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, upForce);
+                jumpCount++; // Increment jump count
+            }
+
+            // Reduce jump height when releasing Space while moving up
+            if (Input.GetKeyUp(KeyCode.Space) && rb.velocity.y > 0f)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            }
+
+            Flip();
+        }
     }
 
     private void FixedUpdate()
@@ -39,6 +59,7 @@ public class Moving : MonoBehaviour
 
     private bool IsGrounded()
     {
+        // This method checks if the player is on the ground layer
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 
@@ -52,15 +73,15 @@ public class Moving : MonoBehaviour
             transform.localScale = localScale;
         }
     }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         Destroy(gameObject);
         Destroy(other.gameObject);
         gameOverText.gameObject.SetActive(true);
 
-
-
+        // Mark as dead to prevent further actions
+        isDead = true;
     }
 }
-
 
